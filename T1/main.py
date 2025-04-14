70cc269aa03dcfc8c678c8b3687af713443bd1f4
AzvdoMatheus <matheusjefremovas@gmail.com>
2025-04-14 20:28:34 -0300

    Functional program class

--- T1/main.py
+++ T1/main.py
@@ -3,6 +3,7 @@ from program import Program
 
 def main():
     instructions, data, labels = Reader._load_File("./inputFiles/prog2.txt")
+    print("Labels carregados:", labels)
     program = Program(instructions, data, labels)
     program.execute()
 
--- T1/program.py
+++ T1/program.py
@@ -1,7 +1,7 @@
 class Program:
     def __init__(self, instructions, data, labels):
         self.instructions = instructions         
-        self.labels = labels                     
+        self.labels = labels                  
         self.data = self._parse_data(data)
         self.pc = 0  
         self.acc = 0 
@@ -17,7 +17,7 @@ class Program:
                 label, value = parts
                 parsed_data[label] = int(value)
         return parsed_data
-
+    
     def execute(self):
         while not self.halted and self.pc < len(self.instructions):
             instr = self.instructions[self.pc]
@@ -25,31 +25,68 @@ class Program:
             op = parts[0].upper()
             operand = parts[1] if len(parts) > 1 else None  
 
+            # print(f"PC: {self.pc}, ACC: {self.acc}, Instr: {instr['instruction']}, Controle: {self.data.get('controle', 'N/A')}")
+
             if op == "LOAD":
                 self._load(operand)
+            elif op == "DIV":
+                self._div(operand)
+            elif op == "MULT":
+                self._mult(operand)
             elif op == "ADD":
                 self._add(operand)
             elif op == "STORE":
                 self._store(operand)
+            elif op == "SUB":
+                self._sub(operand)
             elif op == "SYSCALL":
                 self._syscall(int(operand))
             elif op in ["BRANY", "BRZERO", "BRPOS", "BRNEG"]:
                 self._jump(op, operand)
+                continue  
             else:
-                raise ValueError(f"Operação desconhecida: {op}")
+                raise ValueError(f"Unknown instruction: {op}")
 
             self.pc += 1  
 
+        print("\nEstado final da área de dados:")
+        for key, value in self.data.items():
+            print(f"{key}: {value}")
+
+    def _sub(self, operand):
+        if operand.startswith("#"):  
+            self.acc -= int(operand[1:])
+        else:  
+            self.acc -= self.data.get(operand, 0)
+        
+    def _div(self, operand):
+        if operand.startswith("#"):  
+            divisor = int(operand[1:])
+        else:  
+            divisor = self.data.get(operand, 0)
+
+        if divisor == 0:
+            raise ZeroDivisionError("Division by zero.")
+        else:
+            self.acc //= divisor
+
+    def _mult(self, operand):
+        if operand.startswith("#"):  
+            self.acc *= int(operand[1:])
+        else:  
+            self.acc *= self.data.get(operand, 0)
+
+
     def _jump(self, op, operand):
-        value = self.acc
+        accumulator = self.acc
 
         if op == "BRANY":
             self.pc = self.labels[operand]
-        elif op == "BRZERO" and value == 0:
+        elif op == "BRZERO" and accumulator == 0:
             self.pc = self.labels[operand]
-        elif op == "BRPOS" and value > 0:
+        elif op == "BRPOS" and accumulator > 0:
             self.pc = self.labels[operand]
-        elif op == "BRNEG" and value < 0:
+        elif op == "BRNEG" and accumulator < 0:
             self.pc = self.labels[operand]
         else:
             self.pc += 1
@@ -69,8 +106,6 @@ class Program:
     def _store(self, operand):
         if operand in self.data: 
             self.data[operand] = self.acc
-        else:
-            raise ValueError(f"Variável desconhecida: {operand}")
 
     def _syscall(self, code):
         if code == 0: 
@@ -82,4 +117,4 @@ class Program:
             for key, value in self.data.items():
                 print(f"{key}: {value}")
         else:
-            raise ValueError(f"Código de syscall desconhecido: {code}")
+            raise ValueError(f"Unknown syscall code: {code}")
